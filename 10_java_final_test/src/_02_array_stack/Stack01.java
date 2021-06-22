fc22126a2add87c590ef746ca0d2ebc1d0306b20
잉송영 <@LAPTOP-B5E5718U>
2021-06-22 14:52:44 +0900

    06/22

--- 10_java_final_test/src/_02_array_stack/Stack01.java
+++ 10_java_final_test/src/_02_array_stack/Stack01.java
@@ -22,7 +22,10 @@ class ArrayStack { // 클래스 생성
 		return (top == -1);
 	}
 	// 1) top == -1 이 참일 경우 true 출력
-	//     1 == -1 거짓  ->  false 반환
+	//     0 == -1 거짓  ->  false 반환
+	// 2)  1 == -1 거짓  ->  false 반환
+	// 3)  2 == -1 거짓  ->  false 반환
+	// 4)  1 == -1 거짓  ->  false 반환
 	
 	public boolean isFull(){
 		return (top == this.stackSize-1);
@@ -31,19 +34,23 @@ class ArrayStack { // 클래스 생성
 	//     -1 == 5-1
 	//     -1 == 4 거짓 -> false 반환
 	// 2)   0 == 4 거짓 -> false 반환
+	// 3)   1 == 4 거짓 -> false 반환
 	
-	public void push(char item){ // 1)  A대입 // 2) B대입
+	public void push(char item){ // 1)  A대입 // 2) B대입 // 3) C대입
 		if(isFull()){ // isFull이 참일 경우 성립
 			System.out.println("Inserting fail! Array Stack is full!!");
 		}
-		else{ // 1)은 isFull이 false -> else에 성립함.	
+		else{ // 1) 은 isFull이 false -> else에 성립함.	
 			  // 2) isFull이 false -> 성립
-			itemArray[++top] = item; // 1) itemArray[-1 +1] = 'A' -> itemArray[0] = 'A' 대입
+			  // 3) isFull이 false -> 성립
+			itemArray[++top] = item; // 1) itemArray[0] = 'A' -> itemArray[0] = 'A' 대입
 			                         //    top = -1 -> ++top -> top = top+1 -> top = 0
-			                         // 2) itemArray[1 + 0] = 'B' -> itemArray[1] = 'B' 대입
+			                         // 2) itemArray[1] = 'B' -> itemArray[1] = 'B' 대입
 			                         //    top = 0 -> top = top+1 -> top=1
-			System.out.println("top: " + top); //확인
-			System.out.println("Inserted Item : " + item); // Inserted Item : A 출력됨.
+									 // 3) itemArray[2] = 'C' -> itemArray[2] = 'C' 대입
+			 						 //    top = 1 -> top = top+1 -> top=2
+			System.out.println("[확인용] top: " + top); // 확인용
+			System.out.println("Inserted Item : " + item); 
 		}
 	}
 	
@@ -52,8 +59,10 @@ class ArrayStack { // 클래스 생성
 			System.out.println("Deleting fail! Array Stack is empty!!");
 			return 0;
 		}
-		else{ 
-			return itemArray[top--];	
+		else{ // 4) isEmpty는 false 이므로 else문에 성립함.
+			System.out.println("[확인용2] top: " + top); // 확인용
+			return itemArray[top--]; // 현재 top=2 -> itemArray[2] = C
+			                         // top 2 - 1 =1
 		}				
 	}
 	
@@ -76,12 +85,12 @@ class ArrayStack { // 클래스 생성
 	}
 	
 	public void printStack(){
-		if(isEmpty()) // isEmpty가 참일 경우 성립 
+		if(isEmpty()) // isEmpty가 true일 경우 성립 
 			System.out.printf("Array Stack is empty!! %n %n");
-		else{ // isEmpty가 거짓이므로 성립함.
+		else{ // 1)2)3) false로 else문에 성립
 			System.out.printf("Array Stack>> ");
-			for(int i=0; i<=top; i++) // i=0; i<0 i++ -> i=0일때만 성립
-				System.out.printf("%c ", itemArray[i]); // itemArry[0] = A
+			for(int i=0; i<=top; i++) // 1) i=0; i<=0 i++ -> i=0일때만 성립 // 2) i=0; i<=1; i++ -> i=0,1일때 성립 // 3) i=0; i<=2; i++ -> i=0,1,2 일때 성립 // 4)i=0; i<=1; i++ -> i=0,1일때 성립
+				System.out.printf("%c ", itemArray[i]); // 1) itemArray[0] = A // 2) itemArray[0] = A, [1] = B // 3) itemArray[0] = A, [1] = B, [2]= C // 4)  2) itemArray[0] = A, [1] = B 
 			System.out.println();	System.out.println();
 		}
 	}
@@ -97,16 +106,18 @@ public class Stack01{
 		S.push('A');     // Inserted Item : A 출력
 		S.printStack();  // "Array Stack>> A 출력
 		
-		//2021/06/21 18:48 ~ 
-		S.push('B');
-		S.printStack();
+		//2021/06/21 18:48 ~ 19:30
+		S.push('B');     // Inserted Item : B 출력
+		S.printStack();  // "Array Stack>> A B 출력
 		
-		S.push('C');
-		S.printStack();
+		//2021/06/21 23:39 ~ 23:55
+		S.push('C');     // Inserted Item : C 출력
+		S.printStack();  // "Array Stack>> A B C 출력
 		
-		deletedItem = S.pop();
+		//2021/06/22 00:03 ~ 00:16
+		deletedItem = S.pop(); // Inserted Item : C 출력
 		if(deletedItem != 0)
 			System.out.println("deleted Item : " + deletedItem);
-		S.printStack();		
+		S.printStack();	 // "Array Stack>> A B C 출력	
 	}
 }
--- 10_java_final_test/src/_03_list_stack/ListStack.java
+++ 10_java_final_test/src/_03_list_stack/ListStack.java
@@ -1,6 +1,6 @@
 package _03_list_stack;  //중요!
 
-interface Stack{
+interface Stack{ // 인터페이스 : 추상클래스보다 더 추상화된 클래스, 실제 구현 X 기본 설계도
 	
 	boolean isEmpty();
 	void push(char item);
@@ -19,21 +19,31 @@ class StackNode{
 }
 
 
-class LinkedStack implements Stack{
+class LinkedStack implements Stack{ // implements 키워드를 사용하여 인터페이스를 구현 (상속X)
 	
-	private StackNode top;
+	private StackNode top; // StackNode 클래스 변수 top 생성
 		
 	public boolean isEmpty(){
-		return (top == null);
-	}
+		System.out.println("[확인용] top.data: " + top.data); //확인용
+		return (top == null); // 1) top == null -> false 
+	}                         //    top와 newNode 주소 공유 -> top.data = A
+	                          // 2) top == null -> false
+	                          //    top.data = B
 	
 	
-	public void push(char item){
+	public void push(char item){ // 1) A 대입  /  2) B 대입
 		StackNode newNode = new StackNode();
-		newNode.data = item;
-		newNode.link = top;
-		top = newNode;
-		System.out.println("Inserted Item : " + item);
+		newNode.data = item; // 1) newNode.data에 A 대입  2) B대입
+		newNode.link = top;  // 1)2) newNOde.link에 top 주소 공유
+		System.out.println("[주소 확인용]top : " + top); //확인용
+		System.out.println("[주소 확인용]newNode.link : " + newNode.link); //확인용
+		System.out.println("[주소 확인용]newNode : " + newNode); //확인용
+		top = newNode;       // 1)2) top에 newNode 주소 공유
+		System.out.println("[주소 확인용]top : " + top); //확인용
+		System.out.println("[주소 확인용]newNode : " + newNode); //확인용
+		System.out.println("[확인용2] top.data: " + top.data); //확인용
+		System.out.println("[확인용2] newNode.data: " + newNode.data); //확인용
+		System.out.println("Inserted Item : " + item); // 1) Inserted Item : A // 2) B
 	}
 	
 	
@@ -74,14 +84,16 @@ class LinkedStack implements Stack{
 	}
 	
 	public void printStack(){
-		if(isEmpty())
+		if(isEmpty()) 
 			System.out.printf("Linked Stack is empty!! %n %n");
-		else{
-			StackNode temp = top;
-			System.out.println("Linked Stack>> ");
-			while(temp != null){
-				System.out.printf("\t %c \n", temp.data);
-				temp = temp.link;
+		else{                                              // 1)2) isEmpty -> false로 else문에 성립함. 
+			StackNode temp = top;                          // temp에 top의 주소 공유
+			System.out.println("Linked Stack>> "); 
+			while(temp != null){                           // 1)2) temp와 top의 주소 공유 -> temp != null 성립하여 while문 성립함.
+				System.out.printf("\t %c \n", temp.data);  // 1) temp.data = top.data = 'A' // 2) temp.data = top.data = 'B'
+				temp = temp.link;                          //    temp에 temp.link 공유 -> temp = null;
+				System.out.println("[주소 확인용]top : " + top);  //확인용
+				System.out.println("[주소 확인용]temp : " + temp);//확인용
 			}
 			System.out.println();	
 		}
@@ -91,20 +103,30 @@ class LinkedStack implements Stack{
 public class ListStack{
 	public static void main(String args[]){		
 		char deletedItem;
-		LinkedStack LS = new LinkedStack();
+		LinkedStack LS = new LinkedStack(); // LinkedStack 클래스 변수 LS 생성
 		
-		LS.push('A');
+		//2021/06/22/ 14:04 ~ 14:23
+		LS.push('A');    // Inserted Item : A
+		LS.printStack(); // Linked Stack>> A
+		
+		System.out.println("----------------------------------------------------");
+		
+		//2021/06/22 14:23 ~ 
+		LS.push('B');    // Inserted Item : B
 		LS.printStack();
 		
-		LS.push('B');
-		LS.printStack();
+		System.out.println("----------------------------------------------------");
 		
 		LS.push('C');
 		LS.printStack();
 		
+		System.out.println("----------------------------------------------------");
+		
 		deletedItem = LS.pop();
 		if(deletedItem != 0)
 			System.out.println("deleted Item : " + deletedItem);
-		LS.printStack();		
+		LS.printStack();
+		
+		System.out.println("----------------------------------------------------");
 	}
 }
